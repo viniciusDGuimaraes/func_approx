f3f6b952eeafa6f09b420f7662c7eb1011565989
Vinicius Guimaraes <vinicius.dalmeidaguimaraes@gmail.com>
2025-09-02 22:33:10 -0300

    Changed config.json to previous structure

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from json import loads
 
 from function_factory import FunctionFactory
 from dataset_generator import DatasetGenerator
-from model import Model
+from model_classes.model import Model
 
 
 logger = logging.getLogger(__name__)
@@ -13,29 +13,28 @@ CONFIG_FILEPATH = "./config.json"
 
 
 def validate_config(config: dict) -> bool:
+    # Check if expected keys are present
     assert "functions" in config, "Key 'functions' was not found in config dictionary"
     assert "hidden_states" in config, "Key 'hidden_states' was not found in config dictionary"
     assert "epochs" in config, "Key 'epochs' was not found in config dictionary"
+    assert "start_range" in config, "Key 'start_range' was not found in config dictionary"
+    assert "end_range" in config, "Key 'end_range' was not found in config dictionary"
+    assert "function_step" in config, "Key 'function_step' was not found in config dictionary"
 
+    # Check if values are of the expected type
     assert type(config["functions"]) is list, "Value of 'functions' is not a list"
-    assert all(type(x) is dict for x in config["functions"]), "At least one value of 'functions' is not a dict"
-
-    assert all("function_name" in x for x in config["functions"]), "Key 'function_name' not found in one of the functions dictionaries"
-    assert all(type(x["function_name"]) is str for x in config["functions"]), "At least one value of 'function_name' is not a string"
-
-    assert all("start_range" in x for x in config["functions"]), "Key 'start_range' not found in one of the functions dictionaries"
-    assert all(type(x["start_range"]) is int for x in config["functions"]), "At least one value of 'start_range' is not a integer"
-
-    assert all("end_range" in x for x in config["functions"]), "Key 'end_range' not found in one of the functions dictionaries"
-    assert all(type(x["end_range"]) is int for x in config["functions"]), "At least one value of 'end_range' is not a integer"
-
-    assert all("function_step" in x for x in config["functions"]), "Key 'function_step' not found in one of the functions dictionaries"
-    assert all(type(x["function_step"]) is int for x in config["functions"]), "At least one value of 'function_step' is not a integer"
-
+    assert all(type(x) is str for x in config["functions"]), "At least one value of 'functions' is not a string"
     assert type(config["hidden_states"]) is list, "Value of 'hidden_states' is not a list"
-    assert all(type(x) is int for x in config["hidden_states"]), "At least one value of 'hidden_states' is not a integer"
+    assert all(type(x) is int for x in config["hidden_states"]), "At least one value of 'hidden_states' is not an integer"
+    assert type(config["epochs"]) is int, "Value of 'epochs' is not an integer"
+    assert type(config["start_range"]) is int, "Value of 'start_range' is not an integer"
+    assert type(config["end_range"]) is int, "Value of 'end_range' is not an integer"
+    assert type(config["function_step"]) is float, "Value of 'function_step' is not a float"
 
-    assert type(config["epochs"]) is int, "Value of 'epochs' is not a integer"
+    # Check if values are valid
+    assert config["epochs"] > 0, "Value of 'epochs' must be greater than 0."
+    assert config["end_range"] > config["start_range"], "Value of 'end_range' must be greater than value of 'start_range'."
+    assert config["function_step"] < (config["end_range"] - config["start_range"]), "Value of 'function_step' must be lesser than the difference between 'end_range' and 'start_range'."
 
 
 if __name__ == "__main__":
@@ -49,14 +48,13 @@ if __name__ == "__main__":
     with open(CONFIG_FILEPATH, mode='r') as f:
         config = loads(f.read())
 
-    #validate_config(config)
+    validate_config(config)
 
-    for function_config, hidden_state in itertools.product(config["functions"], config["hidden_states"]):
-        function_name = function_config["function_name"]
-        start_range = function_config["start_range"]
-        end_range = function_config["end_range"]
-        function_step = function_config["function_step"]
+    start_range = config["start_range"]
+    end_range = config["end_range"]
+    function_step = config["function_step"]
 
+    for function_name, hidden_state in itertools.product(config["functions"], config["hidden_states"]):
         log_message = f"Training new model\nHidden state: {hidden_state}\nFunction '{function_name}'\nFunction step: {function_step}"
         print(log_message)
         logger.info(log_message)
@@ -70,7 +68,7 @@ if __name__ == "__main__":
 
         raw_data = fn.generate_data((start_range, end_range), function_step)
 
-        dataset = DatasetGenerator.generate_dataset(raw_data, 0.99, 0.01)
+        dataset = DatasetGenerator.generate_dataset(raw_data, 0.75, 0.15)
 
         model = Model(hidden_state)
         model.train(dataset, config["epochs"], output_filepath, len(raw_data))
